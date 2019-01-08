40a23c425958e3747908902bb9bc198841f57776
le2xx <le2x@rambler.ru>
2019-01-08 22:51:24 +0300

    Created animation in slide component.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,4 +17,46 @@ window.onload = () => {
 	};
 
 	textarea.addEventListener('keydown', autosize);
+
+
+	const slide = document.querySelector('.slide__input');
+
+	const autoParking = () => {
+		const steps = [0, 19, 49, 100];
+		const value = Number(slide.getAttribute('value'));
+		const thumbValue = Number(slide.value);
+		let newValue = 0;
+
+		const slideAnimationLeft = (begin, end) => {
+			if (begin <= end || begin < 0 || end > 100) {
+				return;
+			}
+			setTimeout(() => {
+				slide.value = begin - 1;
+				slide.setAttribute('value', begin - 1);
+				slideAnimationLeft(begin - 1, end);
+			}, 10);
+		};
+
+		const slideAnimationRight = (begin, end) => {
+			if (begin >= end || begin < 0 || end > 100) {
+				return;
+			}
+			setTimeout(() => {
+				slide.value = begin + 1;
+				slide.setAttribute('value', begin + 1);
+				slideAnimationRight(begin + 1, end);
+			}, 10);
+		};
+
+		if (value >= thumbValue) {
+			newValue = steps.filter(step => step < thumbValue).reverse()[0];
+			slideAnimationLeft(thumbValue, newValue);
+		}else {
+			newValue = steps.filter(step => step > thumbValue)[0];
+			slideAnimationRight(thumbValue, newValue);
+		}
+	};
+
+	slide.addEventListener('mouseup', autoParking);
 };
